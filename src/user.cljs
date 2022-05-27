b6354c0b0bf60f0d4f3720651af99de30630f68b
Malte Nottmeyer <nottmey@users.noreply.github.com>
2022-05-27 20:23:30 +0200

    formatting

--- src/user.cljs
+++ src/user.cljs
@@ -2,11 +2,12 @@
   (:require app.core))
 
 (defonce reactor nil)
+
 (defn ^:dev/after-load start! []
   (set! reactor (((ns-publics 'app.core) 'app)              ; re-resolve recompiled Photon main
                  #(js/console.log "Reactor success:" %)
                  #(js/console.error "Reactor failure:" %))))
 
 (defn ^:dev/before-load stop! []
-  (when reactor (reactor))
+  (when reactor (reactor))                                  ; teardown
   (set! reactor nil))
