86f1fb80e94ff368b84179d5e60c58484375dd44
Dustin Getz <dustin.getz@gmail.com>
2022-08-10 21:40:53 -0400

    entrypoint - add RCF

--- src/user.clj
+++ src/user.clj
@@ -1,7 +1,7 @@
 (ns user
-  (:require [datomic.client.api.async :as d]
-            datomic.client.api
+  (:require datomic.client.api
             hyperfiddle.photon-jetty-server
+            hyperfiddle.rcf
             [missionary.core :as m]
             shadow.cljs.devtools.api
             shadow.cljs.devtools.server
@@ -19,12 +19,15 @@
   (def datomic-client (datomic.client.api/client {:server-type :dev-local :system "datomic-samples"}))
   (def datomic-conn (datomic.client.api/connect datomic-client {:db-name "mbrainz-subset"}))
   (def server (hyperfiddle.photon-jetty-server/start-server! photon-server-config))
-  (comment (.stop server)))
+  (comment (.stop server))
+  (hyperfiddle.rcf/enable!))
 
 (comment
   "REPL entrypoint"
   (main)
+  (hyperfiddle.rcf/enable!)
 
+  (require '[datomic.client.api.async :as d])
   (def db (d/db datomic-conn))
   (m/? (app.core/query
          '[:find (pull ?tx [:db/id :db/txInstant])
