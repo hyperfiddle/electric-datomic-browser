4fd67dae0757f9f78dba8d75e6c00864550f77c7
Dustin Getz <dustin.getz@gmail.com>
2023-08-06 12:55:38 -0400

    fix uberjar build logger classpath issue

--- src/prod.clj
+++ src/prod.clj
@@ -2,13 +2,17 @@
   (:gen-class)
   (:require app.datomic-browser
             [app.server :refer [start-server!]]
-            clojure.string))
+            clojure.string
+            [clojure.tools.logging :as log]))
 
 (def electric-server-config
   {:host "0.0.0.0", :port 8080, :resources-path "public"})
 
+(def app-version (System/getProperty "HYPERFIDDLE_ELECTRIC_SERVER_VERSION"))
+
 (defn -main [& args] ; run with `clj -M -m prod`
-  (when (clojure.string/blank? (System/getProperty "HYPERFIDDLE_ELECTRIC_SERVER_VERSION"))
+  (log/info "Starting Electric server, app version: " app-version)
+  (when (clojure.string/blank? app-version)
     (throw (ex-info "HYPERFIDDLE_ELECTRIC_SERVER_VERSION jvm property must be set in prod" {})))
   (start-server! electric-server-config))
 
